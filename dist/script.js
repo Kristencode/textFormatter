df4a5f7c9ac2c3fcc77e96f48a988aca3962549a
Kristen <harunakristen@gmail.com>
2025-06-02 10:42:52 +0000

    test done

---
+++ dist/script.js
@@ -0,0 +1,13 @@
+let button = document.querySelector("button");
+
+let body = document.querySelector("body");
+
+button.addEventListener("click", function () {
+  let value = document.querySelector("input").value;
+  // value.style.color='red'
+  body.style.fontWeight = "bold";
+  body.style.background = "red";
+  body.append(value);
+
+  value = "";
+});
