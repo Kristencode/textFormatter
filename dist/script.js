ef8c37789d0dc69c61f036e6ffcd3318f4a6d056
Kristen <harunakristen@gmail.com>
2025-06-04 16:07:04 +0000

    fully functional

--- dist/script.js
+++ dist/script.js
@@ -1,13 +1,35 @@
-let button = document.querySelector("button");
+// task: Let your take an input,
+// convert the input to lowercase or uppercase,
+// let the color of the text be white
+// let the background be black
 
+let input = document.querySelector("input");
+let button = document.querySelector("button");
 let body = document.querySelector("body");
+let main = document.querySelector("#main");
 
 button.addEventListener("click", function () {
-  let value = document.querySelector("input").value;
-  // value.style.color='red'
-  body.style.fontWeight = "bold";
-  body.style.background = "red";
- 
-  body.append(value);
+  let value = input.value;
+ if(value ==''){
+    alert('input cannot be empty')
+    return
+ }
+
+   if (value === value.toLowerCase()) {
+     value = value.toUpperCase();
+   } else {
+     value = value.toLowerCase();
+   }
+
+  let para = document.createElement("p");
+  para.innerHTML = value;
+  main.appendChild(para);
+  para.style.background = "black";
+  para.style.fontWeight = "bold";
+  para.style.color = "white";
+  para.style.border = "1px solid white";
+  para.style.width = "350px";
+  para.style.padding = "2px";
+
  
 });
